179fd9ec19e278fca017ebcd94a267fe47a46952
nicke <nicke@DESKTOP-PIG0BP2>
2018-03-04 17:25:31 +0100

    First Commit

--- lab2.c
+++ lab2.c
@@ -1,20 +1,21 @@
 #include"sorting.h"
 #include"membership_sorted.h"
 
+#define N 15
 
 int main(){
 
-	int arr[] = {10, 15, 1, 3, 0, 20, 80,30, 5,6};
-	int n = sizeof(arr)/sizeof(arr[0]);
+	int arr[N];
+//	int n = sizeof(arr)/sizeof(arr[0]);
 
 	//Quicksort...
-	quickSort(arr, 0, n-1);
+	quickSort(arr, 0, N-1);
 	printf("SortedArray: ");
-	printArray(arr, n);
+	printArray(arr, N);
 
 
 	//Membership Query...
 	int key = 15;
-	int res = BinaryMember(arr, key, n);
+	int res = BinaryMember(arr, key, N);
 	printf("res = %d \n", res);
 }
